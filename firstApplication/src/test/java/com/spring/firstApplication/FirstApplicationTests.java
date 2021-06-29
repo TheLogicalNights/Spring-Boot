05189b71bda6b6dd484e73ce2ded35de2a2d8591
TheLogicalNights <swapniladhav560@gmail.com>
2021-06-29 18:03:29 +0530

    Added URL redirection

---
+++ firstApplication/src/test/java/com/spring/firstApplication/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.firstApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
