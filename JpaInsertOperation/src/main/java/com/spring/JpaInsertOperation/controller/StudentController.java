985913d434ed8581a5e3ab22e79c7d043f7e9956
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-03 23:05:38 +0530

    Added controller

--- JpaInsertOperation/src/main/java/com/spring/JpaInsertOperation/controller/StudentController.java
+++ JpaInsertOperation/src/main/java/com/spring/JpaInsertOperation/controller/StudentController.java
@@ -1,5 +1,22 @@
 package com.spring.JpaInsertOperation.controller;
 
-public class StudentController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.JpaInsertOperation.Model.StudentModel;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+	
+	@PostMapping
+	public ResponseEntity<String> createuser(@RequestBody StudentModel studentObj)
+	{
+		return new ResponseEntity<String>("Student created successfully",HttpStatus.OK);
+		
+	}
 }
